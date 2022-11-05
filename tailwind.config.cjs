e8437f518a04ef48b02f482d4cfe017dde3b857b
PiotrekPKP <peterpkppeter@gmail.com>
2022-11-05 12:29:13 +0100

    feat: base for landing page

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        orangy: "#FF7A00",
+        purply: "#E962FF",
+        darky: "#201F25",
+      },
+      fontFamily: {
+        "press-start": ['"Press Start 2P"', "cursive"],
+      },
+    },
+  },
+  plugins: [],
+};
